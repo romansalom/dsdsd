7accd9f6a12429d6341b8594c17d95908d62902b
romansalom <roman.salom12@gmail.com>
2023-01-13 16:19:54 -0300

    primeros metodos

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/carts', require("./routers/cartsRouters"));
+app.use('/api', require("./routers/productsRouters.js"));
 
-const PORT = 8080;
+const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log("servidor fuinciona");
 });
---
+++ routers/productsRouters.js
@@ -0,0 +1,38 @@
+
+const express = require("express");
+const routerProducts = express.Router();
+const fs = require("fs");
+
+const productos =[];
+
+const todoslosProductos = JSON.parse(fs.readFileSync("./productos.json" , "utf8", (error)=>{
+    throw Error(error)
+}));
+
+productos.push(...todoslosProductos);
+
+routerProducts.get("/productos", async (req,res)=>{
+    const limit = req.query.limit;
+    if(limit) return res.send(productos.slice(0,limit))
+    res.send(productos)
+});
+
+routerProducts.post("/productos" , async (req,res) =>{
+    const nuevoProducto = req.body
+    productos.push(nuevoProducto);
+    fs.writeFileSync("./productos.json" , JSON.stringify(productos),(err)=>{
+        const productos ={
+            nombre: this.nombre ,
+            precio : this.precio , 
+            id: ProductManager.id
+        };
+        throw new Error(err)
+        
+    })
+
+    res.send("objeto creado ")
+})
+
+module.exports =
+    routerProducts;
+  
