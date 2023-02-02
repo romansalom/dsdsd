317d6bf2042f1b1182c92e44cb8c6c22caee00e8
romansalom <roman.salom12@gmail.com>
2023-02-01 21:08:06 -0300

    entrga sockt

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const express = require("express");
+const handlebars = require('express-handlebars');
+const {Server} = require('socket.io');
 const app = express();
+const fs = require("fs");
+const { v4: uuidv4} = require('uuid');
 const productsRouters = require('./routers/productsRouters');
 const cartsRouters = require('./routers/cartsRouters');
+const views = require("./routers/views");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products',productsRouters);
 app.use('/api/cart',cartsRouters);
-
+app.use('/' , views);
 const PORT = 4000;
-app.listen(PORT, () => {
-  console.log("servidor fuinciona");
+const httpServer = app.listen(PORT,() =>{
+  console.log('servidor levantado ')
 });
+const socketServer = new Server(httpServer);
+
+app.engine('handlebars' , handlebars.engine());
+app.set('views' , __dirname +'/views');
+app.set('view engine' , 'handlebars');
+app.use(express.static(__dirname +'/public'));
+
+
+socketServer.on('connection',  (socket)  =>{
+
+  let products =  JSON.parse(fs.readFileSync("./productos.json" , 'utf-8'));
+
+  socket.emit('cargadeProductos' , products)
+  socket.on("productoNuevo" , (productos)=>{
+    
+    let objetoNuevo = {id:uuidv4() , ...productos};
+    products.push(objetoNuevo);
+
+    fs.writeFileSync("./productos.json" , JSON.stringify(products) )
+
+    socket.emit('cargadeProductos' , products);
+  })
+  
+
+
+
+})
